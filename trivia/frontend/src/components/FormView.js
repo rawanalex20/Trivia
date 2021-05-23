3c732331fda263c55bb1e00a0e56ec83a79eb24e
rawanalex20 <63899074+rawanalex20@users.noreply.github.com>
2021-05-24 01:45:32 +0200

    Add files via upload

--- trivia/frontend/src/components/FormView.js
+++ trivia/frontend/src/components/FormView.js
@@ -92,7 +92,7 @@ class FormView extends Component {
             <select name="category" onChange={this.handleChange}>
               {Object.keys(this.state.categories).map(id => {
                   return (
-                    <option key={id} value={id}>{this.state.categories[id]}</option>
+                    <option key={id} value={ Number(id) + 1}>{this.state.categories[id]}</option>
                   )
                 })}
             </select>
